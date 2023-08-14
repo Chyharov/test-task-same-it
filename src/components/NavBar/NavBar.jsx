6e75896ed82c8421aa76f605e54c3af66f641468
myusername <chyharov@gmail.com>
2023-08-14 20:44:46 +0300

    Header rdy

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,8 +4,8 @@ import s from './NavBar.module.scss';
 const NavBar = () => {
     return (
         <nav className={s.navBar}>
-            <button type='button'><Link to="/">Перевірити ТТН</Link></button>
-            <button type='button'><Link to="/DepartmentsList">Список відділень</Link></button>
+            <button className={s.button__nav} type='button' aria-label='go to home page'><Link to="/">Перевірити ТТН</Link></button>
+            <button className={s.button__nav} type='button' aria-label='go to nova poshta page'><Link to="/DepartmentsList">Список відділень</Link></button>
         </nav>
   )
 };
